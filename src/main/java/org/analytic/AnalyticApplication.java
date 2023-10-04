156a49030f43f2445bb125bb96414b78a62e4ef9
laptev_8 <gleb.laptev.00@bk.ru>
2023-10-04 19:01:28 +0200

    logs

--- src/main/java/org/analytic/AnalyticApplication.java
+++ src/main/java/org/analytic/AnalyticApplication.java
@@ -2,18 +2,24 @@ package org.analytic;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class AnalyticApplication {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(AnalyticApplication.class);
+
   public static void main(String[] args) {
-    System.out.println("test teamcity");
+    LOGGER.info("starting app laptev glev");
     SpringApplication.run(AnalyticApplication.class, args);
+    LOGGER.info("app started gleb laptev");
   }
 
   @Bean
